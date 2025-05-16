b2ca6e89dc6376ddbc6272e28a56aa4876285f95
Garrett Crowe <garrett.crowe@marriott.com>
2025-05-16 16:25:56 -0500

    Status

--- src/components/dialog/OrderConfirmationDialog.tsx
+++ src/components/dialog/OrderConfirmationDialog.tsx
@@ -1,5 +1,28 @@
-import { Dialog, DialogContent } from "@mui/material";
+import { Dialog, DialogContent, Paper, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
+import { useState } from "react";
+import LinearProgressBar from "../progress/LinearProgressBar";
 import CenteredDialogTitle from "./CenteredDialogTitle";
+
+interface RowProps {
+    name: string;
+    start: boolean;
+}
+
+function Row(props: RowProps) {
+    const { name, start } = props;
+    const [install, setInstall] = useState(false);
+
+    return (
+            <TableRow>
+                <TableCell sx={{width: .25}}>
+                    <Typography>{name}</Typography>
+                </TableCell>
+                <TableCell>
+                    <LinearProgressBar start={start} onComplete={() => setInstall(false)} />
+                </TableCell>
+            </TableRow>
+    );
+}
 
 type Props = {
 open: boolean;
@@ -8,10 +31,18 @@ open: boolean;
 
 const OrderConfirmationDialog:React.FC<Props> = ({open, onClose}) => {
     return (
-        <Dialog open={open} onClose={onClose}>
+        <Dialog open={open} onClose={onClose} fullWidth>
             <CenteredDialogTitle title={"Order Status"}/>
             <DialogContent>
-                
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableBody>
+                            <Row name={'Hardware Request'} start={true}/>
+                            <Row name={'Software Request'} start={true}/>
+                            <Row name={'Permission Request'} start={true}/>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </DialogContent>
         </Dialog>
     );
